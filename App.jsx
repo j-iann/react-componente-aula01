04a5762f7f6a794f3a083112d517cbb9616f3961
ian <jesperiansantos.2021@gmail.com>
2021-11-05 16:34:07 -0300

    enviando arquivos

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import  { Message } from './Message.js';
+
+export default function App() {
+  return (
+    <div className="App">
+      
+      <Message />
+    </div>
+  );
+};
+
+ 
---
+++ Message.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Message = () => (
+    <div>
+        <h1>Olá mundo</h1>
+        <h1>Meu nome é Júlia</h1>
+    </div>
+);
